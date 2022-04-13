a353cb4c59d91ff9ce24e0ed6a37f29b0ba5c6ba
Roman <evglevskyroman@gmail.com>
2022-04-13 17:38:37 +0300

    Выполнено первое ДЗ

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,50 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 4 ;
+        int b = 5 ;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else  {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 86 ;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        }
+        if (value > 100) {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 20 ;
+        int b = 15 ;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+}
+
